59a5e19e922d6800dd31e5829a11063de76269d2
Gabriel Schorsch Amadeu <gabrielschorsch@outlook.com>
2019-09-30 15:29:16 -0300

    up

---
+++ ReactJS/gufos-react/src/pages/Categorias/Categorias.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"
+
+class Categorias extends Component {
+    render() {
+        return (
+            <div>Categorias</div>
+        );
+    }
+}
+
+export default Categorias;

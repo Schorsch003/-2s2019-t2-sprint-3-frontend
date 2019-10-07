29c1a40a0a06cb54b9efcdd7b8a766486959f9c6
Gabriel Schorsch Amadeu <gabrielschorsch@outlook.com>
2019-10-07 15:10:26 -0300

    exercicio tabela fipe adicionado

--- ReactJS/Exercicios/3.frontend.react.tabelafipe/tabelafipe/src/App.js
+++ ReactJS/Exercicios/3.frontend.react.tabelafipe/tabelafipe/src/App.js
@@ -1,15 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <select>
+  constructor() {
+    super();
+    this.state = {
+      listaMarcas: [],
+      listaCarros: {
+        modelos:[]
+      }
+    }
+  }
 
-      </select>
-    </div>
-  );
+  componentDidMount() {
+    this.recuperarMarcas();
+  }
+
+
+  recuperarMarcas = () => {
+    fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas')
+      .then(x => x.json())
+      .then(x => this.setState({ listaMarcas: x }));
+  }
+
+  setIdMarca = (event) => {
+    // this.setState({ idSelecionado:  })
+    console.log(event.target.value)
+    // console.log(this.state.idSelecionado)
+    this.recuperarModelos(event.target.value)
+    
+    
+  }
+
+  recuperarModelos = (id) => {
+    fetch('https://parallelum.com.br/fipe/api/v1/carros/marcas/' + id + '/modelos')
+      .then(x => x.json())
+      .then(x => this.setState({listaCarros: x}));
+  }
+  // this.setState({ listaCarros: x })
+
+
+
+  render() {
+    return (
+      <div className="App" >
+        <select onChange={this.setIdMarca}>
+          <option selected disabled>Marcas</option>
+          {this.state.listaMarcas.map(x => {
+            return (
+              <option key={x.codigo} value={x.codigo} >{x.nome}</option>
+            );
+          })}
+        </select>
+        <select>
+          <option selected disabled>Modelos</option>
+          {this.state.listaCarros.modelos.map(x => {
+            return (
+              <option key={x.codigo}>{x.nome}</option>
+            )
+          })}
+        </select>
+      </div>
+    );
+  }
 }
 
 export default App;
